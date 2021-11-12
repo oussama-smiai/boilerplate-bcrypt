325e5cb14c16703fac40b989a600821eb9dc18d1
oussama smiai <smiai.oussama@gmail.com>
2021-11-12 16:23:31 +0000

    bcrypt

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt =require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-const bcrypt =require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
