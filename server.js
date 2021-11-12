d63664d2f078e608c5f97564211434e09193f74f
oussama smiai <smiai.oussama@gmail.com>
2021-11-12 16:49:01 +0000

    compare hash

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  console.log(hash);
+  //console.log(hash);
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-    console.log(res);
+    //console.log(res);
   });
 });
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
