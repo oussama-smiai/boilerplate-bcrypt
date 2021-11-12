208f12d1c12d44c8775b6176ae3699171b81b293
oussama smiai <smiai.oussama@gmail.com>
2021-11-12 16:55:47 +0000

    change

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  //console.log(hash);
+  bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+    //console.log(res);
+  });
+});
+
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 
